2234d7ce0a1310805430417bca5361bc569f3386
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-10-30 14:52:39 +0000

    header comp + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import { Header}  from './components/Header'
 
 function App() {
   return (
     <>
-        <h1>M'yellow!</h1>
+        <Header />
         <p> Placeholder</p>
     </>
   )
