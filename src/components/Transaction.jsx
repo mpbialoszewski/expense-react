76a0426139a785775278e3094770e84e5d0bb2dc
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-10-30 16:55:11 +0000

    mapping over all transactions and putting appropriate class based on value

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext  } from './context/GlobalState'
 
 export const Transaction = ({ transaction } ) => {
 
+  const { deleteTransaction } = useContext(GlobalContext)
     const sign = transaction.amount < 0 ? '-' : '+' 
   return (
     <div>
-        <li class={transaction.amount < 0 ? 'minus' : 'plus'}>
-          {transaction.text} <span>{sign}£{Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+          {transaction.text} <span>{sign} £{Math.abs(transaction.amount.toFixed(2))}</span><button
+          onClick={()=> deleteTransaction(transaction.id)}
+          className="delete-btn">x</button>
         </li>
     </div>
   )
