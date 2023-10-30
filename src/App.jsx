cbc350e716e36802edda95fea11c1dfe18dd651f
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-10-30 16:03:56 +0000

    Adding global provider for data

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import { Balance}  from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
+
+
+import { GlobalProvider } from './components/context/GlobalState';
+
+
 function App() {
   return (
-    <>
+    <div>
+    <GlobalProvider>
         <Header />
         <Balance />
         <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
-    </>
-  )
-}
+    </GlobalProvider>
+    </div>
+)}
 
 export default App
